7abf47c18e2abc280f958c0df6d2830c46def8b0
Ayush-2301 <ayushchauhan233@gmail.com>
2024-03-20 00:58:23 +0530

    fixed minor changes

--- PCS24-56/App/frontend/src/components/Hero.jsx
+++ PCS24-56/App/frontend/src/components/Hero.jsx
@@ -1,28 +1,12 @@
-import { Container, Card, Button } from "react-bootstrap";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
-  return (
-    <div className=" py-5">
-      <Container className="d-flex justify-content-center">
-        <Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
-          <h1 className="text-center mb-4">MERN Authentication</h1>
-          <p className="text-center mb-4">
-            This is a boilerplate for MERN authentication that stores a JWT in
-            an HTTP-Only cookie. It also uses Redux Toolkit and the React
-            Bootstrap library
-          </p>
-          <div className="d-flex">
-            <Button variant="primary" href="/login" className="me-3">
-              Sign In
-            </Button>
-            <Button variant="secondary" href="/register">
-              Register
-            </Button>
-          </div>
-        </Card>
-      </Container>
-    </div>
-  );
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/login");
+  });
+  return <div className=" py-5"></div>;
 };
 
 export default Hero;
